783537d27dd6ad1e9b9668a33d0f915b66328543
geekontheway <zxl51@qq.com>
2012-02-09 12:22:37 +0800

    add cancan

---
+++ app/models/ability.rb
@@ -0,0 +1,28 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(admin)
+    # Define abilities for the passed in admin here. For example:
+    #
+    #   admin ||= admin.new # guest admin (not logged in)
+    #   if admin.admin?
+    #     can :manage, :all
+    #   else
+    #     can :read, :all
+    #   end
+    #
+    # The first argument to `can` is the action you are giving the admin permission to do.
+    # If you pass :manage it will apply to every action. Other common actions here are
+    # :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the admin can perform the action on. If you pass
+    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the objects.
+    # For example, here the admin can only update published articles.
+    #
+    #   can :update, Article, :published => true
+    #
+    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+  end
+end
